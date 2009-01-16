fd5e95c9162c6033ca8ca4a61abf6680c64f61f6
Jonathan Feinberg <jdf@pobox.com>
2009-01-16 01:11:11 +0000

    Added a reader for .bpt files, and put the teapot into the distro, because, you know.

---
+++ src/patchy/readers/BPTReader.java
@@ -0,0 +1,54 @@
+package patchy.readers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import patchy.Patch;
+import patchy.PatchGroup;
+import patchy.Patchy;
+import processing.core.PVector;
+
+public class BPTReader
+{
+	public static PatchGroup read(final InputStream inputStream) throws IOException
+	{
+		int line = 1;
+		final BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+		try
+		{
+			final int patchCount = Integer.parseInt(in.readLine());
+			line++;
+			final PatchGroup result = new PatchGroup();
+			for (int i = 0; i < patchCount; i++)
+			{
+				final String[] dimensions = in.readLine().split("\\s+");
+				if (dimensions.length != 2)
+					throw new RuntimeException("Expected patch dimensions at line "
+							+ line);
+				if (!(dimensions[0].equals("3") && dimensions[1].equals("3")))
+					throw new RuntimeException("I can only parse bicubic patches, not "
+							+ dimensions[0] + "," + dimensions[1]);
+				final PVector[][] cp = new PVector[4][4];
+				for (int x = 0; x < 4; x++)
+					for (int y = 0; y < 4; y++)
+					{
+						final String[] coords = in.readLine().split("\\s+");
+						cp[x][y] = new PVector(Float.parseFloat(coords[0]), Float
+								.parseFloat(coords[1]), Float.parseFloat(coords[2]));
+					}
+				result.add(Patch.create(Patchy.BEZIER, cp));
+			}
+			return result;
+		}
+		catch (NumberFormatException e)
+		{
+			throw new RuntimeException(e + " on line " + line);
+		}
+		finally
+		{
+			in.close();
+		}
+	}
+}
